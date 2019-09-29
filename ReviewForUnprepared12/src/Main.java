dedfe76420552a66c9b6a761a6aca41faca94b3c
OmenM <55457267+OmenM@users.noreply.github.com>
2019-09-29 13:11:09 -0700

    asd

--- ReviewForUnprepared12/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        MainMenu mainMenu = new MainMenu();
-        while(mainMenu.cont){
-            mainMenu.mainMenuStart();
-        }
-    }
-}
