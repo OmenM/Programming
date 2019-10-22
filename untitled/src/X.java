862b9d13c750d0715fd88e38e2c51f07a97a4530
OmenM <55457267+OmenM@users.noreply.github.com>
2019-10-22 11:37:53 -0700

    fd

---
+++ untitled/src/X.java
@@ -0,0 +1,18 @@
+public class X {
+    void method(int a){
+        System.out.println("ONE");
+    }
+    void method(double d){
+        System.out.println("TWO");
+    }
+
+}
+class Y extends X
+{
+    @Override
+    void method(double d)
+    {
+        System.out.println("THREE");
+    }
+}
+
