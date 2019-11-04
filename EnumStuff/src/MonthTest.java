cc199864f5ba8dd4fb17684f399e45c66428715a
OmenM <55457267+OmenM@users.noreply.github.com>
2019-11-04 11:21:22 -0800

    Month

---
+++ EnumStuff/src/MonthTest.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class MonthTest {
+    Month month;
+
+    public MonthTest (Month month){
+        this.month = month;
+    }
+    public void printMonth(){
+        switch(month){
+
+            case JANUARY:
+                System.out.println(month);
+                break;
+            case FEBRUARY:
+                System.out.println(month);
+                break;
+            case MARCH:
+                System.out.println(month);
+                break;
+            case APRIL:
+                System.out.println(month);
+                break;
+            case MAY:
+                System.out.println(month);
+                break;
+            case JUNE:
+                System.out.println(month);
+                break;
+            case JULY:
+                System.out.println(month);
+                break;
+            case AUGUST:
+                System.out.println(month);
+                break;
+            case SEPTEMBER:
+                System.out.println(month);
+                break;
+            case OCTOBER:
+                System.out.println(month);
+                break;
+            case NOVEMBER:
+                System.out.println(month);
+                break;
+            case DECEMBER:
+                System.out.println(month);
+                break;
+        }
+    }
+
+
+}
